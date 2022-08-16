b78e1b77cfa3eb2e499f3d683ccf3c833bd29de5
Daniel1992 <86936468+danielfloresd1992@users.noreply.github.com>
2022-08-15 22:27:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const btnClose = document.getElementById('toogle-close');
 const menu = document.getElementById('menu');
+let key = null;
+window.addEventListener('DOMContentLoaded', e => {
+    btnClose.addEventListener('click', () => {
+        menu.classList.toggle('opend');
+    });
+});
 
 function textAnimate(v1, v2, v3,boolean){
     const barra1 = document.querySelectorAll('.hibilitities_barra')[v1];
@@ -11,23 +17,24 @@ function textAnimate(v1, v2, v3,boolean){
     }
     else{
         barra2.classList.add(`animate${v2}`);
-        setInterval(()=>{
-            let barra1Width = window.getComputedStyle(barra1);
+        setInterval(()=>{  
+            const barra1Width = window.getComputedStyle(barra1);
             const barraStyle = window.getComputedStyle(barra2);
             let valor1 = Number(barra1Width.width.substring(0, 7));
             let valor2 = Number(barraStyle.width.substring(0, 7));
-            let result = parseInt(valor2 * 100 / valor1);
-            isNaN(result) ? console.log(`aqui vale result: ${result}`) : indiceTect.textContent =`${result}%`;
-            console.log(result)
+            let result = valor2 * 100 / 920;
+    
+            isNaN(result) ? console.log(`aqui vale result: ${result}`) : indiceTect.textContent =`${parseInt(result)}%`;
+            setTimeout(() => {
+                clearInterval();
+                clearTimeout();
+            }, 5000);
         },100);
     }
 }
 
-window.addEventListener('DOMContentLoaded', e => {
-    btnClose.addEventListener('click', () => {
-        menu.classList.toggle('opend');
-    });
-});
+
+
 
 const mainBarra = document.querySelector('.main--hibilitities');
 const options = {
@@ -41,12 +48,15 @@ observer.observe(mainBarra);
 
 function handleIntersect(entries){
     entries.forEach(entry => {
+        console.log(entry);
         if(entry.intersectionRatio*100 > 1){
             textAnimate(0,0,0, false);
             textAnimate(1,1,1, false);
             textAnimate(2,2,2, false);
+            key = false;
         }
         else if(entry.intersectionRatio*100 < 1){
+            key = true;
             textAnimate(0,0,0, true);
             textAnimate(1,1,1, true);
             textAnimate(2,2,2, true);
